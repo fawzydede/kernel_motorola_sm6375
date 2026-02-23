bc0cfb9ff224ddaa3b7f234c0a24a0ebe62d3219
dodyirawan85 <dev.irawans@gmail.com>
2026-02-23 23:45:28 +0700

    Revert "input: touchscreen: ilitek_v3_mmi: Disable dt-gesture sensor when using dt2w control"
    This reverts commit f9f3b469dae841ef053634c0c6e7512bfe12b0ca.
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- drivers/input/touchscreen/ilitek_v3_mmi/ilitek_v3_qcom.c
+++ drivers/input/touchscreen/ilitek_v3_mmi/ilitek_v3_qcom.c
@@ -57,7 +57,7 @@ static int ili_tp_power_on_reinit(void)
 }
 #endif
 
-#if defined(ILI_SENSOR_EN) && !defined(ILI_DOUBLE_TAP_CTRL)
+#ifdef ILI_SENSOR_EN
 static struct sensors_classdev __maybe_unused sensors_touch_cdev = {
 
 	.name = "dt-gesture",
@@ -143,9 +143,6 @@ void ili_input_register(void)
 	}
 
 	/* Gesture keys register */
-#ifdef ILI_DOUBLE_TAP_CTRL
-	input_set_capability(ilits->input, EV_KEY, KEY_WAKEUP);
-#endif
 	input_set_capability(ilits->input, EV_KEY, KEY_POWER);
 	input_set_capability(ilits->input, EV_KEY, KEY_GESTURE_UP);
 	input_set_capability(ilits->input, EV_KEY, KEY_GESTURE_DOWN);
@@ -161,9 +158,6 @@ void ili_input_register(void)
 	input_set_capability(ilits->input, EV_KEY, KEY_GESTURE_C);
 	input_set_capability(ilits->input, EV_KEY, KEY_GESTURE_F);
 
-#ifdef ILI_DOUBLE_TAP_CTRL
-	__set_bit(KEY_WAKEUP, ilits->input->keybit);
-#endif
 	__set_bit(KEY_GESTURE_POWER, ilits->input->keybit);
 	__set_bit(KEY_GESTURE_UP, ilits->input->keybit);
 	__set_bit(KEY_GESTURE_DOWN, ilits->input->keybit);
@@ -758,10 +752,13 @@ static void ilitek_plat_late_resume(struct early_suspend *h)
 }
 #endif/*defined(CONFIG_FB) || defined(CONFIG_DRM_MSM)*/
 
-#if defined(ILI_SENSOR_EN) && !defined(ILI_DOUBLE_TAP_CTRL)
+#ifdef ILI_SENSOR_EN
 static int ili_sensor_set_enable(struct sensors_classdev *sensors_cdev,
 		unsigned int enable)
 {
+#ifdef ILI_DOUBLE_TAP_CTRL
+	ILI_DBG("double tap ctrl, do nothing\n");
+#else
 	ILI_INFO("Gesture set enable %d!", enable);
 	if (enable == 1) {
 		ilits->should_enable_gesture = true;
@@ -770,6 +767,7 @@ static int ili_sensor_set_enable(struct sensors_classdev *sensors_cdev,
 	} else {
 		ILI_INFO("unknown enable symbol\n");
 	}
+#endif
 	return 0;
 }
 
@@ -797,6 +795,9 @@ static int ili_sensor_init(struct ilitek_ts_data *data)
 	if (data->report_gesture_key) {
 		__set_bit(EV_KEY, sensor_input_dev->evbit);
 		__set_bit(BTN_TRIGGER_HAPPY3, sensor_input_dev->keybit);
+#ifdef ILI_DOUBLE_TAP_CTRL
+		__set_bit(BTN_TRIGGER_HAPPY6, sensor_input_dev->keybit);
+#endif
 	} else {
 		__set_bit(EV_ABS, sensor_input_dev->evbit);
 		input_set_abs_params(sensor_input_dev, ABS_DISTANCE,
@@ -941,7 +942,7 @@ err_register_disp_notif_failed:
 
 static int ilitek_plat_probe(void)
 {
-#if defined(ILI_SENSOR_EN) && !defined(ILI_DOUBLE_TAP_CTRL)
+#ifdef ILI_SENSOR_EN
 	static bool initialized_sensor;
 #endif
 #if SUSPEND_RESUME_SUPPORT
@@ -999,7 +1000,7 @@ static int ilitek_plat_probe(void)
 #endif
 #endif
 
-#if defined(ILI_SENSOR_EN) && !defined(ILI_DOUBLE_TAP_CTRL)
+#ifdef ILI_SENSOR_EN
 	if (!initialized_sensor) {
 #ifdef CONFIG_HAS_WAKELOCK
 		wake_lock_init(&(ilits->gesture_wakelock), WAKE_LOCK_SUSPEND, "dt-wake-lock");
